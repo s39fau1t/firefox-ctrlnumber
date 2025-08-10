b53bc361c4e4314c3fcbd35caf64281228390bc6
Daniel Richardson <der07921@gmail.com>
2025-08-10 16:16:48 -0400

    Fixed to behave correctly with tab groups

--- src/ctrlnumber.js
+++ src/ctrlnumber.js
@@ -1,11 +1,33 @@
-browser.commands.onCommand.addListener(async function(command) {
-    let idx = command.slice(-1) % 9 - 1;
-    const tabs = await browser.tabs.query({ currentWindow: true, hidden: false});
-    if (tabs.length <= idx) {
-        idx = tabs.length - 1
+function* reverse(arr) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        yield arr[i];
     }
-    const tab = tabs.slice(idx);
-    if (tab.length) {
-        browser.tabs.update(tab[0].id, { active: true });
+}
+
+browser.commands.onCommand.addListener(async command => {
+    let idx = parseInt(command.slice(-1));
+
+    let tabs = await browser.tabs.query({ currentWindow: true, hidden: false });
+
+    if (idx === 9 || idx >= tabs.length) {
+        tabs = reverse(tabs);
+
+        idx = 1;
+    }
+
+    for (const tab of tabs) {
+        if (tab.groupId !== -1) {
+            const group = await browser.tabGroups.get(tab.groupId);
+
+            if (group.collapsed) {
+                continue;
+            }
+        }
+
+        if (--idx === 0) {
+            browser.tabs.update(tab.id, { active: true });
+
+            break;
+        }
     }
 });
